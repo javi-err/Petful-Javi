02342fa6b2e2400b30f5c596497433a93476cce9
javi <jsmart1993@hotmail.com>
2020-07-12 00:25:55 -0700

    changes

--- modules/app/app.js
+++ modules/app/app.js
@@ -8,5 +8,7 @@ app.use(cors());
 app.use("/people", require("../people/people.router"));
 app.use("/dog", require("../dog/dog.router"));
 app.use("/cat", require("../cat/cat.router") )
-
+app.get("/", (req, res) => {
+  return res.send("hi");
+});
 module.exports = app;
