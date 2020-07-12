60915a9210e41ad4e42d7fa9fb52cc21da59001e
javi <jsmart1993@hotmail.com>
2020-07-12 00:24:24 -0700

    changes

--- modules/app/app.js
+++ modules/app/app.js
@@ -1,26 +1,12 @@
-const express = require('express')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('../../config')
-const app = express()
+const express = require("express");
+const cors = require("cors");
 
-app.use(helmet())
+const app = express();
+
 app.use(cors());
 
-app.use('/api/people', require('../people/people.router'))
-app.use('/api/cat', require('../cat/cat.router'))
-app.use('/api/dog', require('../dog/dog.router'))
+app.use("/people", require("../people/people.router"));
+app.use("/dog", require("../dog/dog.router"));
+app.use("/cat", require("../cat/cat.router") )
 
-app.use((error, req, res, next) => {
-  let message
-  if (NODE_ENV === 'production') {
-    message = 'Server error'
-  }
-  else {
-    console.error(error)
-    message = error.message
-  }
-  res.status(500).json(message)
-})
-
-module.exports = app
+module.exports = app;
