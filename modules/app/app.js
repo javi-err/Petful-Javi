bcc7ea58e7b9cf219a8b78338936dcf52d6b6b35
javi <jsmart1993@hotmail.com>
2020-07-12 01:59:58 -0700

    changes

--- modules/app/app.js
+++ modules/app/app.js
@@ -1,13 +1,17 @@
-const express = require("express");
-const cors = require("cors");
+  
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 app.use(cors());
 
-app.use('/api/people', require('../people/people.router'))
-app.use('/api/cat', require('../cat/cat.router'))
-app.use('/api/dog', require('../dog/dog.router'))
+app.use('/people', require('../people/people.router'));
+app.use('/pets', require('../pets/pets.router'));
 
+// request handling
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, world! This is the Petful Server!');
+});
 
 module.exports = app;
--- modules/pets/pets.router.js
+++ modules/pets/pets.router.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const json = require('body-parser').json()
 
-const Cat = require('./cat.service')
+const Pets = require('./pets.service')
+const People = require('../people/people.service')
+
 const router = express.Router()
 
 router.get('/', (req, res) => {
   // Return all pets currently up for adoption.
-  const cat = Cat.get()
-  return res.json(cat)
+  return res.status(200).json(Pets.get())
 })
 
-router.delete('/', (req, res) => {
+router.delete('/', json, (req, res) => {
   // Remove a pet from adoption.
-  const cat = Cat.dequeue()
-  return res.json(cat)
+  Pets.dequeue(req.body.type);
+  People.dequeue();
+  res.status(204).end();
 })
 
 module.exports = router
