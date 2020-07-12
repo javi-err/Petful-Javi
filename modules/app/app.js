c88f2d6dca5143911b32a92e1419713f52d0ce86
javi <jsmart1993@hotmail.com>
2020-07-12 00:06:44 -0700

    changes

--- modules/app/app.js
+++ modules/app/app.js
@@ -1,12 +1,30 @@
+  
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-
+const helmet = require('helmet')
+const { CLIENT_ORIGIN, NODE_ENV } = require('../../config')
 const app = express()
 
+app.use(helmet())
 app.use(cors({
-  }))
+  origin: CLIENT_ORIGIN
+}));
 
 app.use('/api/people', require('../people/people.router'))
-app.use('/api/pets', require('../pets/pets.router'))
+app.use('/api/cat', require('../cat/cat.router'))
+app.use('/api/dog', require('../dog/dog.router'))
+
+app.use((error, req, res, next) => {
+  let message
+  if (NODE_ENV === 'production') {
+    message = 'Server error'
+  }
+  else {
+    console.error(error)
+    message = error.message
+  }
+  res.status(500).json(message)
+})
 
 module.exports = app
---
+++ modules/cat/cat.service.js
@@ -0,0 +1,24 @@
+  
+const Queue = require('../queue/Queue')
+const store = require('../../store')
+
+// Set up initial data.
+// --------------------
+
+const cats = new Queue()
+store.cats.forEach(cat => cats.enqueue(cat))
+
+// --------------------
+
+module.exports = {
+  get() {
+    // Return the pets to be adopted.
+    return cats.showNext()
+  },
+
+  dequeue() {
+    // Remove a pet from the queue.
+    cats.dequeue()
+    return cats.showNext()
+  }
+}
